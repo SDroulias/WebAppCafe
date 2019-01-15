33679b37c5f1ec02ed20f92627d573745a88952d
stathis-st <stathis.stamatis@gmail.com>
2019-01-15 22:54:01 +0200

    Implements view product (not solved)

--- src/com/webappcafe/dao/ProductDAOImpl.java
+++ src/com/webappcafe/dao/ProductDAOImpl.java
@@ -3,18 +3,20 @@ package com.webappcafe.dao;
 import com.webappcafe.datasource.Database;
 import com.webappcafe.model.Product;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductDAOImpl implements ProductDAO {
 
-    private static final String INSERT_STATEMENT = String.format("INSERT INTO %s (%s, %s, %s) VALUES(?, ?, ?);",
+    private static final String INSERT_PRODUCT_STATEMENT = String.format("INSERT INTO %s (%s, %s, %s) VALUES(?, ?, ?);",
             "`products`", "`name`", "`description`", "`price`");
 
     //TODO final String UPDATE_STATEMENT
+
+//    public static final String UPDATE_STATEMENT = String.format("UPDATE %s SET ");
+
+    public static final String SELECT_PRODUCT_STATEMENT = String.format("SELECT * FROM %s;", "`products`");
 
     private Database database = Database.getInstance();
 
@@ -27,7 +29,7 @@ public class ProductDAOImpl implements ProductDAO {
         ResultSet resultSet = null;
 
         try {
-            preparedStatement = connection.prepareStatement(INSERT_STATEMENT, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement = connection.prepareStatement(INSERT_PRODUCT_STATEMENT, Statement.RETURN_GENERATED_KEYS);
             preparedStatement.setString(1, product.getName());
             preparedStatement.setString(2, product.getDescription());
             preparedStatement.setDouble(3, product.getPrice());
@@ -54,5 +56,25 @@ public class ProductDAOImpl implements ProductDAO {
             }
         }
         return id;
+    }
+
+    public List<Product> getAllProducts() {
+        List<Product> productList = new ArrayList<>();
+        try (Connection connection = database.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_PRODUCT_STATEMENT);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            resultSet.beforeFirst();
+            while (resultSet.next()) {
+                Product product = new Product();
+                product.setId(resultSet.getLong(1));
+                product.setName(resultSet.getString(2));
+                product.setDescription(resultSet.getString(3));
+                product.setPrice(resultSet.getDouble(4));
+                productList.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productList;
     }
 }
---
+++ web/resources/js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+  const $editForm = $("#edit-form");
+  const $submitBtn = $("#submit-edit");
+  const $editBtn = $(".edit-btn");
+  const $formId = $("#product-id");
+  const $formName = $("#product-name");
+  const $formPrice = $("#product-price");
+  const $formDescr = $("#product-descr");
+
+  $editBtn.click(function(){
+    $id = $(this).parent().siblings(".id");
+    $name = $(this).parent().siblings(".name");
+    $price = $(this).parent().siblings(".price");
+    $descr = $(this).parent().siblings(".descr");
+
+    $formId.val($id.html());
+
+    $formName.prop("disabled",false);
+    $formName.val($name.html());
+
+    $formPrice.prop("disabled",false);
+    $formPrice.val(parseFloat($price.html()));
+
+    $formDescr.prop("disabled",false);
+    $formDescr.val($descr.html());
+
+    $submitBtn.prop("disabled", false);
+
+  });
+
+  $editForm.submit(function(e){
+    e.preventDefault();
+    this.submit();
+    setTimeout(function(){
+      $submitBtn.prop("disabled", true);
+      $formId.prop("disabled",true);
+      $formName.prop("disabled",true);
+      $formPrice.prop("disabled",true);
+      $formDescr.prop("disabled",true);
+      $formId.val('');
+      $formName.val('');
+      $formPrice.val('');
+      $formDescr.val('');
+    }, 1);
+
+  });
+});
