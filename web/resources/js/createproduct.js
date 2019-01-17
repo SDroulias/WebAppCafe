24d3e120f2d055b1c5a159a9f2ebd3b32aefd882
Fredericia <39918560+stevD9@users.noreply.github.com>
2019-01-17 22:55:49 +0200

    Fixed modal and redirect after create product

---
+++ web/resources/js/createproduct.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  const $createBtn = $("#create-btn");
+  const $modal = $("#successModal");
+  const $form = $("#create-form");
+  const $productName = $("#product-name");
+  const $productPrice = $("#product-price");
+  const $productDescr = $("#product-descr");
+
+  $createBtn.click(function(e){
+    e.preventDefault();
+    $form.addClass("was-validated");
+    if ($form[0].checkValidity()){
+      $.post("createProduct",{productName: $productName.val(), price: $productPrice.val(), description: $productDescr.val()},
+      function(){
+        $modal.modal('show');
+      });
+    }
+  });
+
+});
