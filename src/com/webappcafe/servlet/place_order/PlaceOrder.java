3add82976cb457dc5f8e9973e40b9f39b443dffb
pagidas <pagakrivos@gmail.com>
2019-01-20 21:58:06 +0200

    Creates servlet for placing order, parsing JSON info from POST method

---
+++ src/com/webappcafe/servlet/place_order/PlaceOrder.java
@@ -0,0 +1,28 @@
+package com.webappcafe.servlet.place_order;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "placeOrder", urlPatterns = {"/placeOrder"})
+public class PlaceOrder extends HttpServlet {
+
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        
+    }
+
+}
